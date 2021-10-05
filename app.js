54538ba81140ae2df94c10d8f3eaf09b4269dc8d
Ash Porter <ashley.porter@eyecuelab.com>
2021-10-05 14:03:59 -0700

    add views and app config

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
 const app = express();
-app.listen();
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
