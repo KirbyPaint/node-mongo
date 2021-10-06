ca563e4c7955595533ea96cf10fdefeba0c4be0f
Ash Porter <ashley.porter@eyecuelab.com>
2021-10-05 18:22:41 -0700

    add styling, google auth, passport

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const path = require("path");
+const passport = require("passport");
 
 // Load config
 dotenv.config({ path: "./config/config.env" });
+
+// Passport Config
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -20,6 +25,13 @@ if (process.env.NODE_ENV === "development") {
 // Handlebars
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
+
+// Passport middleware
+app.use(passport.initialize);
+app.use(passport.session);
+
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
